22f6acf35acf818f1311d15531773deaa5417964
Unknown <jp.knap@gmail.com>
2018-09-17 13:27:58 -0300

    Auto stash before revert of "Merge remote-tracking branch 'origin/juan' into juan"

---
+++ database/migrations/2018_09_17_042941_create_provincias_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProvinciasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('provincias', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('description');
+            $table->integer('region_id')->unsigned();
+            $table->foreign('region_id')->references('id')->on('regiones');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('provincias');
+    }
+}

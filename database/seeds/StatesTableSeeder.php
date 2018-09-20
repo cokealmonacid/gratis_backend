eacc7b20f0b4a8307ad79ef2b1a2377745d4a397
Juan Pablo Muñoz <jp.knap@gmail.com>
2018-09-20 11:06:54 -0300

    seeders of tables has been created

---
+++ database/seeds/StatesTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class StatesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('states')->insert([
+            'description' => str_random(10)
+        ]);
+    }
+}

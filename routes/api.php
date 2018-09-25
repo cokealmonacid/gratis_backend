e998f62485f7942c80bf3f51106166f5b0b944ee
Jorge Almonacid <jorgealmonacid24@gmail.com>
2018-09-24 23:28:17 -0300

    Added route, added UsersControllers and method to login users, modificate the auth file on config directory for use the new namespace of model user

--- routes/api.php
+++ routes/api.php
@@ -14,5 +14,5 @@ use Illuminate\Http\Request;
 */
 
 Route::group(['prefix' => 'v1', 'namespace' => 'API'], function() {
-	Route::get('/prueba', 'TestController@test');
+	Route::post('/users/login', 'UsersController@login');
 });

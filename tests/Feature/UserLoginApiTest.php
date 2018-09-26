f0cb2796a9df0f7498e92060d39e9844c65354c5
Jorge Almonacid <jorgealmonacid24@gmail.com>
2018-09-25 23:18:14 -0300

    Added email does not exist test

--- tests/Feature/UserLoginApiTest.php
+++ tests/Feature/UserLoginApiTest.php
@@ -8,11 +8,29 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UserLoginApiTest extends TestCase
 {
+	use WithFaker;
+
 	/** @test */ 
 	public function it_throws_login_validation_error()
 	{
 		$response = $this->postJson('api/v1/users/login');
 
 		$response->assertStatus(422);
+	}
+
+	/** @test */
+	public function it_throws_email_account_does_not_exist()
+	{
+		$email    = $this->faker->freeEmail();
+		$password = $this->faker->word();
+
+		$data     = [
+			'email'    => $email,
+			'password' => $password 
+		];
+
+		$response = $this->postJson('api/v1/users/login', $data);
+
+		$response->assertStatus(400);
 	}
 }

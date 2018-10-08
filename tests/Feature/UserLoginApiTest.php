192018b478dce00a6ec078d18ffb3471cf81a899
Jorge Almonacid <jorgealmonacid24@gmail.com>
2018-10-08 15:06:19 -0300

    Revert "Added email has no user rol test"
    This reverts commit a3da914c9b48e4149d379eb349989ab563c8140f.

--- tests/Feature/UserLoginApiTest.php
+++ tests/Feature/UserLoginApiTest.php
@@ -5,9 +5,6 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use App\Models\User;
-use App\Models\User_Rol;
-use App\Models\Rol;
 
 class UserLoginApiTest extends TestCase
 {
@@ -34,36 +31,6 @@ class UserLoginApiTest extends TestCase
 
 		$response = $this->postJson('api/v1/users/login', $data);
 
-		$response->assertStatus(400);
-	}
-
-	/** @test */
-	public function it_throws_email_has_no_user_role()
-	{
-		$email    = $this->faker->freeEmail();
-		$password = $this->faker->word();
-		$name     = $this->faker->firstNameMale();
-
-		$user = User::create([
-			'email'    => $email,
-			'password' => $password,
-			'name'     => $name
-		]);
-
-		$rol = Rol::where('description', 'admin')->first();
-
-		User_rol::create([
-			'user_id' => $user->id->toString(),
-			'rol_id'  => $rol->id
-		]);
-
-		$data     = [
-			'email'    => $email,
-			'password' => $password 
-		];
-
-		$response = $this->postJson('api/v1/users/login', $data);
-		
 		$response->assertStatus(400);
 	}
 }

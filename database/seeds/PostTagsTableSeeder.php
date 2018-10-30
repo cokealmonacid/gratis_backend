d71d806007dbb2e25cc65dc486da4cf44c2f638d
Juan Pablo Muñoz <jp.knap@gmail.com>
2018-10-29 21:09:28 -0300

    Busqueda pagina de post mediante filtros (titulo o provincia_id o region_id o tag_id )

---
+++ database/seeds/PostTagsTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use App\Models\Post;
+use App\Models\Post_Tags;
+use App\Models\Tag;
+use App\Models\State;
+
+
+class PostTagsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    	$faker = Faker::create();
+
+    	for ($i=0; $i < 100; $i++) {
+
+    		$tag 		= Tag::inRandomOrder()->first();
+    		$post       = Post::inRandomOrder()->first();
+
+    		Post_Tags::create([
+    			'post_id'      => $post->id,
+    			'tag_id'     => $tag->id
+            ]);
+    	}
+    }
+}

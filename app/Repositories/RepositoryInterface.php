bf7c92f274644e85b3f7f1018e5d2225937f92bf
Juan Pablo Muñoz <jp.knap@gmail.com>
2018-12-10 16:55:11 -0300

    implement Repository interface create User Repository Interface create User Repository

---
+++ app/Repositories/RepositoryInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: entropia
+ * Date: 12/10/18
+ * Time: 3:28 PM
+ */
+
+namespace App\Repositories;
+
+
+interface RepositoryInterface
+{
+
+}
---
+++ app/Repositories/UserRepositoryInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: entropia
+ * Date: 12/10/18
+ * Time: 3:32 PM
+ */
+
+namespace App\Repositories;
+
+
+interface UserRepositoryInterface
+{
+
+}

aa1aa05c751289c669578be58cfd479299f43cfe
Juan Pablo Muñoz <jp.knap@gmail.com>
2018-12-19 20:16:47 -0300

    Implement repository User

--- tests/Feature/UserLoginApiTest.php
+++ tests/Feature/UserLoginApiTest.php
@@ -13,29 +13,113 @@ use Hash;
 
 class UserLoginApiTest extends TestCase
 {
-	use WithFaker, RefreshDatabase;
+    use WithFaker, RefreshDatabase;
     /** @test */
-	public function it_throws_login_validation_error()
-	{
-		$response = $this->postJson('api/v1/users/login');
+    public function it_throws_login_validation_error()
+    {
+        $response = $this->postJson('api/v1/users/login');
 
-		$response->assertStatus(422);
-	}
+        $response->assertStatus(422);
+    }
 
     /** @test */
-	public function it_throws_email_account_does_not_exist()
-	{
-		$email    = $this->faker->freeEmail();
-		$password = $this->faker->word();
+    public function it_throws_email_account_does_not_exist()
+    {
+        $email    = $this->faker->freeEmail();
+        $password = $this->faker->word();
 
-		$data     = [
-			'email'    => $email,
-			'password' => $password 
-		];
+        $data     = [
+            'email'    => $email,
+            'password' => $password
+        ];
 
-		$response = $this->postJson('api/v1/users/login', $data);
+        $response = $this->postJson('api/v1/users/login', $data);
 
-		$response->assertStatus(400);
-	}
+        $response->assertStatus(400);
+    }
 
+    /** @test */
+    public function it_throws_email_has_no_user_role()
+    {
+        $email    = $this->faker->freeEmail();
+        $password = $this->faker->word();
+
+        $user = User::create([
+            'email'    => $email,
+            'password' => $password,
+        ]);
+
+        $rol = Rol::where('description', 'admin')->first();
+
+        User_rol::create([
+            'user_id' => $user->id,
+            'rol_id'  => $rol->id
+        ]);
+
+        $data     = [
+            'email'    => $email,
+            'password' => $password
+        ];
+
+        $response = $this->postJson('api/v1/users/login', $data);
+
+        $response->assertStatus(400);
+    }
+
+    /** @test */
+    public function it_throws_email_and_password_dont_match()
+    {
+        $user = User::first();
+
+        $data = [
+            'email'    => $user->email,
+            'password' => $user->email
+        ];
+
+        $response = $this->postJson('api/v1/users/login', $data);
+
+        $response->assertStatus(400);
+    }
+
+    /** @test */
+    public function it_login_a_user()
+    {
+        $email    = $this->faker->freeEmail();
+        $password = $this->faker->word();
+
+        $user = User::create(
+            [
+                'email'     => $email,
+                'password'  => bcrypt($password)
+            ]
+        );
+
+        $rol = Rol::where('description', 'user')->first();
+
+        User_rol::create([
+            'user_id' => $user->id,
+            'rol_id'  => $rol->id
+        ]);
+
+        $data     = [
+            'email'    => $email,
+            'password' => $password
+        ];
+        $response = $this->postJson('api/v1/users/login',["email" =>"{$email}", "password" => "{$password}"]);
+
+        $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function it_logout_a_user(){
+        /** login */
+        $user = factory(User::class)->create();
+
+        Passport::actingAs($user,['api']);
+
+        $response = $this->postJson('api/v1/users/logout',[]);
+
+        $response->assertStatus(200);
+
+    }
 }

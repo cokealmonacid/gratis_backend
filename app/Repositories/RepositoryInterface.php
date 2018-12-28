30cdb0335cac9ac89316e4abb2b090b6f2504457
Jorge Almonacid <jorgealmonacid24@gmail.com>
2018-12-28 11:42:31 -0300

    Added Repository Interface

--- app/Repositories/RepositoryInterface.php
+++ app/Repositories/RepositoryInterface.php
@@ -1,24 +1,17 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: entropia
- * Date: 12/10/18
- * Time: 3:28 PM
- */
 
 namespace App\Repositories;
 
-
 interface RepositoryInterface
 {
-    public function all();
+	public function all();
 
-    public function create(array $data);
+	public function create(array $data);
 
-    public function update(array $data, $id);
+	public function update(array $data, $id);
 
-    public function delete($id);
+	public function delete($id);
 
-    public function find($id);
-
+	public function find($id);
 }
+

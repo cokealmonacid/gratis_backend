06dc5bc9de74b83477bb74ad14c7710249b633eb
Juan Pablo Muñoz <jp.knap@gmail.com>
2019-04-21 19:44:07 -0400

    -Textos personalizados

--- resources/lang/es/messages.php
+++ resources/lang/es/messages.php
@@ -1,7 +1,14 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: entropia
- * Date: 4/21/19
- * Time: 7:25 PM
- */
+
+return [
+    'user_not_found' => 'Usuario no encontrado',
+    'user_login_error' => 'El correo y la contraseña no coinciden',
+    'user_logout_success' => 'Se ha desconectado con exito',
+    'user_email_not_exist' => 'Esta cuenta de correo no esta registrada',
+    'rol_not_exist' => 'El rol buscado no existe',
+    'post_not_exist' => 'Post buscado no existe',
+    'post_like_error_remove' => 'Error en remover like del post',
+    'post_error_parametros' => 'Se encuentran errores de validacion en los parametros del post',
+    'post_save_error' => 'Se ha producido un error interno al intentar registrar el post'
+
+];

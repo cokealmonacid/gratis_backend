2dc13393a025bd35302616eebd31dc36f090a712
Juan Pablo Muñoz <jp.knap@gmail.com>
2019-04-28 15:57:56 -0400

    Revert "-Textos personalizados"
    This reverts commit 06dc5bc9de74b83477bb74ad14c7710249b633eb.

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -1,14 +1,7 @@
 <?php
-
-return [
-    'user_not_found' => 'User not found',
-    'user_email_not_exist' => 'This email account does not exist',
-    'user_login_error' => 'The email and password dont match',
-    'user_logout_success' => 'Logout success',
-    'rol_not_exist' => 'This rol does not exist',
-    'post_not_exist' => 'Post not exist',
-    'post_like_error_remove' => 'Error in remove like post',
-    'post_error_parametros' => 'Paramaters failed validation for a post',
-    'post_save_error' => 'Error while internally saving an post'
-
-];
+/**
+ * Created by PhpStorm.
+ * User: entropia
+ * Date: 4/21/19
+ * Time: 7:25 PM
+ */

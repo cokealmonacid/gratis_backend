971c0306dc1451dff3137a53e4c4bff75d285e74
Jorge Almonacid <jorgealmonacid24@gmail.com>
2020-02-06 17:18:38 -0300

    removed imageable from rules on facebook

--- app/Models/User.php
+++ app/Models/User.php
@@ -57,7 +57,7 @@ class User extends Authenticatable
             'facebookId'    => 'required',
             'name'          => 'required',
             'email'         => 'required|email',
-            'avatar'        => 'required|imageable',
+            'avatar'        => 'required',
             'facebookToken' => 'required',
         ];
     }
